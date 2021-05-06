147efad29ef60eaa2dc1d2a1c8069a564c6d9362
Ricardo Caceres <rcaceres1125@gmail.com>
2021-05-06 13:14:16 -0600

    Implementar Basic GUI

--- Menu.py
+++ Menu.py
@@ -1,29 +1,37 @@
-'''Se debera importar los archivos de los diferentes filtros con sus respectivos nombres.
-    '''
+from tkinter import *
+#from filtro1 import filtro1
+#from filtro2 import filtro2
+#from filtro3 import filtro3
 
-import filtro1
-import filtro2
-import filtro3
+window = Tk()
 
-def menu():
-    nombre_imagen = input("Ingresa el nombre de la imagen a usar: ")
-    print("\nFILTROS DISPONIBLES")
-    print("1. Filtro 1")
-    print("2. Filtro 2")
-    print("3. Filtro 3")
-    print("4. Salir")
+window.title("Menu de Filtros")
 
-def main():
-    continua = True
-    while continua:
-        menu()
-        opc_menu = int(input())
-        if opc_menu == 1:
-            filtro1(nombre_imagen)
-        elif opc_menu == 2:
-            filtro2(nombre_imagen)
-        elif opc_menu == 3:
-            filtro3(nombre_imagen)
-        elif opc_menu == 4:
-            break
-main()
+window.geometry('400x300')
+
+lbl = Label(window, text="Ingresa el nombre de la imagen")
+
+lbl.grid(column=0, row=0)
+
+txt = Entry(window,width=10)
+
+txt.grid(column=1, row=0)
+
+def clicked():
+   
+    nombre = txt.get()
+    numero = 3
+    lbl.configure(text= "Selecciona uno de los filtros de abajo")
+    boton_filtro1 = Button(window, text="Filtro 1", command=lambda: filtro1(nombre))
+    boton_filtro1.grid(column=0, row=1)
+
+    boton_filtro2 = Button(window, text="Filtro 2", command=lambda: filtro2(nombre))
+    boton_filtro2.grid(column=0, row=2)
+
+    boton_filtro3 = Button(window, text="Filtro 3", command=lambda: filtro3(nombre))
+    boton_filtro3.grid(column=0, row=3)
+ 
+btn = Button(window, text="Ok", command=clicked)
+btn.grid(column=2, row=0)
+
+window.mainloop()
