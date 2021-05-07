602764a3a68b3713826909811ec4e8064b4e4743
Ricardo Caceres <rcaceres1125@gmail.com>
2021-05-06 19:10:11 -0600

    Implementa opciones para multiples filtros

--- Menu.py
+++ Menu.py
@@ -1,6 +1,5 @@
 from tkinter import *
-from filtro import outline
-#Tambien se importaran en las versiones mas actualizadas los demas filtros
+from filtro import filters
 
 window = Tk()
 
@@ -21,28 +20,28 @@ def clicked():
     nombre = txt.get()
     
     lbl.configure(text= "Selecciona uno de los filtros de abajo")
-    boton_filtro1 = Button(window, text="Blur", command=lambda: outline(nombre,1))
+    boton_filtro1 = Button(window, text="Blur", command=lambda: filters(nombre,1))
     boton_filtro1.grid(column=0, row=1)
 
-    boton_filtro2 = Button(window, text="Bottom Solbel", command=lambda: filtro2(nombre,2))
+    boton_filtro2 = Button(window, text="Bottom Solbel", command=lambda: filters(nombre,2))
     boton_filtro2.grid(column=0, row=2)
 
-    boton_filtro3 = Button(window, text="Emboss", command=lambda: filtro3(nombre,3))
+    boton_filtro3 = Button(window, text="Emboss", command=lambda: filters(nombre,3))
     boton_filtro3.grid(column=0, row=3)
 
-    boton_filtro4 = Button(window, text="Left Sobel", command=lambda: filtro3(nombre,4))
+    boton_filtro4 = Button(window, text="Left Sobel", command=lambda: filters(nombre,4))
     boton_filtro4.grid(column=0, row=4)
 
-    boton_filtro5 = Button(window, text="Outline", command=lambda: filtro3(nombre,5))
+    boton_filtro5 = Button(window, text="Outline", command=lambda: filters(nombre,5))
     boton_filtro5.grid(column=0, row=5)
 
-    boton_filtro6 = Button(window, text="Night Sobel", command=lambda: filtro3(nombre,6))
+    boton_filtro6 = Button(window, text="Night Sobel", command=lambda: filters(nombre,6))
     boton_filtro6.grid(column=0, row=6)
 
-    boton_filtro7 = Button(window, text="Sharpen", command=lambda: filtro3(nombre,7))
+    boton_filtro7 = Button(window, text="Sharpen", command=lambda: filters(nombre,7))
     boton_filtro7.grid(column=0, row=7)
 
-    boton_filtro8 = Button(window, text="Top Sobel", command=lambda: filtro3(nombre,8))
+    boton_filtro8 = Button(window, text="Top Sobel", command=lambda: filters(nombre,8))
     boton_filtro8.grid(column=0, row=8)
  
 btn = Button(window, text="Ok", command=clicked)
