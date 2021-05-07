e8512dfa3faff74547e59f3bd1e4eaba65dac780
Diego <A01552594@itesm.mx>
2021-05-06 21:17:36 -0500

    Comentar codigo

--- Menu.py
+++ Menu.py
@@ -1,51 +1,66 @@
+#Importar librerias
 from tkinter import *
 from filtro import filters
 
+#Creacion de ventana
 window = Tk()
-
 window.title("Menu de Filtros")
-
 window.geometry('400x300')
 
 lbl = Label(window, text="Ingresa el nombre de la imagen")
-
 lbl.grid(column=0, row=0)
 
-txt = Entry(window,width=10)
-
+txt = Entry(window, width=10)
 txt.grid(column=1, row=0)
 
+#Funcion de menu de seleccion
 def clicked():
-   
+
     nombre = txt.get()
-    
-    lbl.configure(text= "Selecciona uno de los filtros de abajo")
-    boton_filtro1 = Button(window, text="Blur", command=lambda: filters(nombre,1))
+
+    lbl.configure(text="Selecciona uno de los filtros de abajo")
+    boton_filtro1 = Button(window,
+                           text="Blur",
+                           command=lambda: filters(nombre, 1))
     boton_filtro1.grid(column=0, row=1)
 
-    boton_filtro2 = Button(window, text="Bottom Solbel", command=lambda: filters(nombre,2))
+    boton_filtro2 = Button(window,
+                           text="Bottom Solbel",
+                           command=lambda: filters(nombre, 2))
     boton_filtro2.grid(column=0, row=2)
 
-    boton_filtro3 = Button(window, text="Emboss", command=lambda: filters(nombre,3))
+    boton_filtro3 = Button(window,
+                           text="Emboss",
+                           command=lambda: filters(nombre, 3))
     boton_filtro3.grid(column=0, row=3)
 
-    boton_filtro4 = Button(window, text="Left Sobel", command=lambda: filters(nombre,4))
+    boton_filtro4 = Button(window,
+                           text="Left Sobel",
+                           command=lambda: filters(nombre, 4))
     boton_filtro4.grid(column=0, row=4)
 
-    boton_filtro5 = Button(window, text="Outline", command=lambda: filters(nombre,5))
+    boton_filtro5 = Button(window,
+                           text="Outline",
+                           command=lambda: filters(nombre, 5))
     boton_filtro5.grid(column=0, row=5)
 
-    boton_filtro6 = Button(window, text="Night Sobel", command=lambda: filters(nombre,6))
+    boton_filtro6 = Button(window,
+                           text="Night Sobel",
+                           command=lambda: filters(nombre, 6))
     boton_filtro6.grid(column=0, row=6)
 
-    boton_filtro7 = Button(window, text="Sharpen", command=lambda: filters(nombre,7))
+    boton_filtro7 = Button(window,
+                           text="Sharpen",
+                           command=lambda: filters(nombre, 7))
     boton_filtro7.grid(column=0, row=7)
 
-    boton_filtro8 = Button(window, text="Top Sobel", command=lambda: filters(nombre,8))
+    boton_filtro8 = Button(window,
+                           text="Top Sobel",
+                           command=lambda: filters(nombre, 8))
     boton_filtro8.grid(column=0, row=8)
- 
+
+
 btn = Button(window, text="Ok", command=clicked)
 btn.grid(column=2, row=0)
 
 window.mainloop()
-
--- filtro.py
+++ filtro.py
@@ -1,50 +1,55 @@
+#Importacion de librerias
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-def filters(nombre, opc):
-    img=cv2.imread(nombre)
 
-    imgray=cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+def filters(nombre, opc):#Funcion para la lectura de imagenes y aplicacion de filtros
+    img = cv2.imread(nombre)
+
+    imgray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     forma = np.shape(imgray)
     img2 = np.zeros(forma)
 
-    if opc==1:
-        kernel = np.array([[0.0625, 0.125, 0.0625], [0.125, 0.25, 0.125], [0.0625, 0.125, 0.0625]])
-        n_filter="Imagen con filtro Blur"
-    
-    elif opc==2:
+
+    #Eleccion y definicion de filtros
+    if opc == 1:
+        kernel = np.array([[0.0625, 0.125, 0.0625], [0.125, 0.25, 0.125],
+                           [0.0625, 0.125, 0.0625]])
+        n_filter = "Imagen con filtro Blur"
+
+    elif opc == 2:
         kernel = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
-        n_filter="Imagen con filtro Botton Solbel"
+        n_filter = "Imagen con filtro Botton Solbel"
 
-    elif opc==3:
+    elif opc == 3:
         kernel = np.array([[-2, -1, 0], [-1, 1, 1], [0, 1, 2]])
-        n_filter="Imagen con filtro Emboss"
+        n_filter = "Imagen con filtro Emboss"
 
-    elif opc==4:
+    elif opc == 4:
         kernel = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
-        n_filter="Imagen con filtro Left Sobel"
+        n_filter = "Imagen con filtro Left Sobel"
 
-    elif opc==5:
+    elif opc == 5:
         kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
-        n_filter="Imagen con filtro Outline"
+        n_filter = "Imagen con filtro Outline"
 
-    elif opc==6:
+    elif opc == 6:
         kernel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
-        n_filter="Imagen con filtro Night Sobel"
+        n_filter = "Imagen con filtro Night Sobel"
 
-    elif opc==7:
+    elif opc == 7:
         kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
-        n_filter="Imagen con filtro Sharpen"
+        n_filter = "Imagen con filtro Sharpen"
 
-    elif opc==8:
-        kernel = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])  
-        n_filter="Imagen con filtro Top Sobel"
+    elif opc == 8:
+        kernel = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+        n_filter = "Imagen con filtro Top Sobel"
 
     else:
-        kernel = np.array([[0, 0, 0], [0, 1, -0], [0, 0, 0]])  
+        kernel = np.array([[0, 0, 0], [0, 1, -0], [0, 0, 0]])
 
-
+    #Procedimiento para la convolucion
     for x in list(range(1, forma[0] - 1)):
         for y in list(range(1, forma[1] - 1)):
             suma = 0
@@ -55,11 +60,13 @@ def filters(nombre, opc):
 
             img2[x, y] = suma
 
+    #Ajustes del filtro
     imgfilt = np.stack(img2)
     imgfilt[imgfilt > 255] = 255
     imgfilt[imgfilt < 0] = 0
     imgfilt = imgfilt.astype("uint8")
 
+    #Impresion de imagesnes/graficas
     plt.imshow(img, cmap='gray')
     plt.title("Imagen original")
     plt.show()
@@ -69,5 +76,5 @@ def filters(nombre, opc):
     plt.show()
 
     plt.imshow(imgfilt, cmap='gray')
-    plt.title( n_filter )
+    plt.title(n_filter)
     plt.show()
